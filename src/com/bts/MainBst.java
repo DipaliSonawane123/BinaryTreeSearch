7988bd7b2424d3640af9da24a1a6b3e87692aa5c
Dipali <dipssonawane123@gmail.com>
2022-07-02 02:18:57 +0530

    Modified:Ability ti Add Binary Tree From Figure Feature is modified

--- src/com/bts/MainBst.java
+++ src/com/bts/MainBst.java
@@ -1,25 +1,30 @@
-/*
- *  Ability to create a BST by adding 56 and then adding 30 & 70
- *   Use INode to create My Binary Node
- *   Note the key has to extend comparable to compare and determine left or right node
- *   First add 56 as root node so 30 will be added to left and 70 to right
- */
+/* 2. Ability to create the binary tree shown in the figure
+    *    Check if all are added with using size method in Binary Tree
+      */
 package com.bts;
 public class MainBst {
     public static void main(String[] args) {
         System.out.println("welcome to BTS program");
-
         BinaryTree bst = new BinaryTree();
         Node root = null;
         // inserting the value in root
-        root = bst.insert(root, 56); // 56 is value is adding in root
-
+        root = bst.insert(root, 56);
         root = bst.insert(root, 30);
+        root = bst.insert(root, 70); // 30->56->70->80
 
-        // 30->56->70->80
-        root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
 
         bst.print(root);
 
     }
+
 }
